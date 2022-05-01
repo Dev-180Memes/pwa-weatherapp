33494c091f816a6d7aaea8b0a157ee712b4a4fc4
Dev-180Memes <adeoluwaagbakosi@gmail.com>
2022-05-01 23:57:32 +0000

    Created Service Worker

---
+++ public/serviceworker.js
@@ -0,0 +1,45 @@
+/* eslint-disable array-callback-return */
+const CACHE_NAME = "version-1";
+const urlstoCache = ['index.html', 'offline.html' ];
+
+const self = this;
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlstoCache);
+            })
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
+
+//Activate Service Worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
+});
